82ad53e1d94750846d941f47a3812802aa5e931f
victor <617699485@qq.com>
2018-10-10 14:01:23 +0800

    add icons

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -4,7 +4,7 @@
         <swiper :options="swiperOption">
             <!-- slides -->
             <swiper-slide v-for="item of swiperList" :key="item.id">
-                <img class="swiper-img" :src="item.imgUrl" alt="">
+                <img class="swiper-img" :src="item.imgUrl"  />
             </swiper-slide>
             <!-- Optional controls -->
             <div class="swiper-pagination"  slot="pagination"></div>
@@ -18,15 +18,15 @@ export default {
   data () {
     return {
       swiperOption: {
-        pagination:'.swiper-pagination',
-        loop:true
+        pagination: '.swiper-pagination',
+        loop: true
       },
       swiperList: [{
-        id:'0001',
-        imgUrl:'http://img1.qunarzz.com/piao/fusion/1809/31/da037478f37cf202.jpg_750x200_a02cf862.jpg'
-      },{
-        id:'0002',
-        imgUrl:'http://img1.qunarzz.com/piao/fusion/1808/7e/19b06067afac3402.jpg_750x200_f613313a.jpg'
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/31/da037478f37cf202.jpg_750x200_a02cf862.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1808/7e/19b06067afac3402.jpg_750x200_f613313a.jpg'
       }]
     }
   }
@@ -40,7 +40,7 @@ export default {
      overflow:hidden
      width:100%
      height:0
-     padding-bottom:31.25%
+     padding-bottom:26.25%
      background:#eee
     .swiper-img
         width: 100%
